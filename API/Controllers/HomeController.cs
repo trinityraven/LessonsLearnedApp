d1ef7a822c81aa20848ce4e63ba126d9a765ebc6
Sean Donovan <Sean.Donovan@sennheiser.com>
2020-10-26 17:01:01 -0600

    Add SwaggerUI Code - needs testing and debugging, Swagger elements not displaying

--- API/Controllers/HomeController.cs
+++ API/Controllers/HomeController.cs
@@ -1,37 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using API.Models;
+using System.Diagnostics;
 
 namespace API.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+#pragma warning disable IDE0052 // Remove unread private members
+        private readonly ILogger<HomeController> logger;
+#pragma warning restore IDE0052 // Remove unread private members
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        public HomeController(ILogger<HomeController> logger) => this.logger = logger;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+        public IActionResult Index() => View();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
---
+++ API/Controllers/SampleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        // GET: api/<SampleController>
+        [HttpGet]
+        public IEnumerable<string> Get() => new string[] { "value1", "value2" };
+
+        // GET api/<SampleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id) => "value";
+
+        // POST api/<SampleController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SampleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SampleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
