8971101dffd16259b30ebafe32587f4cb8fb20a7
Sean Donovan <trinityraven@gmail.com>
2020-10-29 21:53:57 -0600

    Add [Key] Id fields

--- DataObjects/DatabaseObjects/Link.cs
+++ DataObjects/DatabaseObjects/Link.cs
@@ -1,9 +1,13 @@
 using DataObjects.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataObjects.DatabaseObjects
 {
     public class Link
     {
+        [Key]
+        public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
         public string URL { get; set; }
--- DataObjects/DatabaseObjects/Project.cs
+++ DataObjects/DatabaseObjects/Project.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataObjects.DatabaseObjects
 {
     public class Project
     {
+        [Key]
+        public int Id { get; set; }
+
         public string Name { get; set; }
         public string Description { get; set; }
 
