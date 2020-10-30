3c9daee835df32ee5289b93c02ab4b8dc5d41d91
Sean Donovan <trinityraven@gmail.com>
2020-10-29 21:44:53 -0600

    Add Project DataObject

---
+++ DataObjects/DatabaseObjects/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DataObjects.DatabaseObjects
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+        public DateTime? EndOfDevelopment { get; set; } = null;
+
+        public DateTime? StartOfSupport => EndOfDevelopment;
+        public DateTime? EndOfSupport { get; set; } = null;
+
+        public DateTime? EnodOfLife { get; set; } = null;
+    }
+}
