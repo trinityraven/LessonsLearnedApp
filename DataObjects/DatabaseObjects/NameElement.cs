2aed0fc0239c37dd9581ab9409de4a18f3662524
Sean Donovan <trinityraven@gmail.com>
2020-10-29 23:01:44 -0600

    Add Name classes and structure

---
+++ DataObjects/DatabaseObjects/NameElement.cs
@@ -0,0 +1,16 @@
+﻿using DataObjects.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DataObjects.DatabaseObjects
+{
+    public class NameElement
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public NameElementType NameType { get; set; }
+        public string Value { get; set; }
+        public int Index { get; set; }
+    }
+}
