15b8899a27d56666431c54d4e53250f3e2efb17c
Sean Donovan <Sean.Donovan@sennheiser.com>
2020-11-17 17:12:02 -0700

    Add LinkController and ILinkProcessor

---
+++ API/Controllers/LinkController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Processing.Interfaces;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinkController : Controller
+    {
+        private readonly ILinkProcessor processor;
+
+        public LinkController(ILinkProcessor linkProcessor) => processor = linkProcessor;
+
+        [HttpGet("link-types")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<Dictionary<int, string>> GetLinkTypesApi() => Ok(processor.GetLinkTypes());
+    }
+}
---
+++ Processing/Interfaces/ILinkProcessor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Processing.Interfaces
+{
+    public interface ILinkProcessor
+    {
+        Dictionary<int, string> GetLinkTypes();
+    }
+}
